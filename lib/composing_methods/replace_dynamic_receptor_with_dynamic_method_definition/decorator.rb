fb01888db0ce6342576e835da0077a590138abdd
DuJust <du.just.it@gmail.com>
2014-06-22 06:02:41 +1000

    [refactoring] Add replace dynamic receptor with dynamic method definition.

--- lib/composing_methods/replace_dynamic_receptor_with_dynamic_method_definition/decorator.rb
+++ lib/composing_methods/replace_dynamic_receptor_with_dynamic_method_definition/decorator.rb
@@ -1,11 +1,12 @@
 module ReplaceDynamicReceptorWithDynamicMethodDefinition
   class Decorator
     def initialize(subject)
-      @subject = subject
-    end
-
-    def method_missing(sym, *args, &block)
-      @subject.send(sym, *args, &block)
+      subject.public_methods.each do |method_name|
+        eigenclass = class << self; self; end
+        eigenclass.send(:define_method, method_name) do |*args|
+          subject.send(method_name, *args)
+        end
+      end
     end
   end
 end
